def12effe31c5fb6db8ef5f8f2afa5697399cad0
sauceTartarys <vladislavserafim81@gmail.com>
2024-02-18 18:28:09 +0200

    ngrd

--- player.py
+++ player.py
@@ -2,12 +2,15 @@ import pygame
 import PyQt5.QtWidgets
 
 class Player():
-    def __init__(self, x, y, w, h, speed, texture, color, parent=None, ):
-        super().__init__(parent)
+    def __init__(self, x, y, w, h, speed, texture, color, ):
         self.speed = speed
         self.texture = pygame.image.load(texture)
-        self.setRect(50, 50, w, h)
-        self.setPos(x, y)
-        self.setBrush(color("blue"))
+        self.hit_box = self.texture.get_rect()
+        self.hit_box.x = x
+        self.hit_box.y = y
+        self.texture = pygame.transform.scale(self.texture, (w, h))
         self.direction = {"up": False, "down": False, "left": False, "right": False}
         self.speed = 5
+    def render(self, window):
+        window.blit(self.texture, (self.hit_box.x, self.hit_box.y))
+
